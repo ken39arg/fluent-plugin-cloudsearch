d306b22155def56b70132f19a99dea38fb83487f
Kensaku Araga <ken39arg@gmail.com>
2015-12-24 16:17:20 +0900

    testを全て書いた

--- test/plugin/test_out_cloudsearch.rb
+++ test/plugin/test_out_cloudsearch.rb
@@ -1,4 +1,8 @@
 require 'helper'
+require 'json'
+require 'aws-sdk'
+
+require 'test/unit/rr'
 
 class CloudSearchOutputTest < Test::Unit::TestCase
   def setup
@@ -7,15 +11,31 @@ class CloudSearchOutputTest < Test::Unit::TestCase
 
   CONFIG = %[
     endpoint http://example.com
+    log_level debug
   ]
 
   def create_driver(conf = CONFIG, tag='test')
     Fluent::Test::BufferedOutputTestDriver.new(Fluent::CloudSearchOutput, tag).configure(conf)
   end
 
+  def create_driver_no_write(conf = CONFIG, tag='test')
+    Fluent::Test::BufferedOutputTestDriver.new(Fluent::CloudSearchOutput, tag) do
+      def write(chunk)
+        chunk.read
+      end
+    end.configure(conf)
+  end
+
   def test_configure
     assert_raise(Fluent::ConfigError) {
       d = create_driver('')
+    }
+
+    assert_raise(Fluent::ConfigError) {
+      d = create_driver %[
+        endpoint http://example.com
+        buffer_chunk_limit 5M
+      ]
     }
 
     d = create_driver %[
@@ -32,9 +52,8 @@ class CloudSearchOutputTest < Test::Unit::TestCase
 
     d = create_driver %[
       endpoint http://example.com
-      buffer_chunk_limit 5M
       access_key_id ackey
-      secret_access_key seckey 
+      secret_access_key seckey
     ]
     assert_equal 4.5*1024*1024, d.instance.buffer.buffer_chunk_limit
     assert_equal nil, d.instance.profile_name
@@ -43,11 +62,83 @@ class CloudSearchOutputTest < Test::Unit::TestCase
 
   end
 
+  def test_credential_type_access_key
+
+    d = create_driver_no_write %[
+      endpoint http://example.com
+      access_key_id ackey
+      secret_access_key seckey
+    ]
+    d.run
+
+    client = d.instance.instance_variable_get(:@client)
+    credentials = client.config.credentials
+
+    assert_instance_of(Aws::Credentials, credentials)
+    assert_equal 'ackey', credentials.access_key_id
+    assert_equal 'seckey', credentials.secret_access_key
+
+  end
+
+  def test_credential_type_profile_name
+
+    mock(Aws::SharedCredentials).new(:profile_name => 'cfprof') {
+      Aws::Credentials.new("ok", "this is shared credentials mock")
+    }
+    d = create_driver_no_write %[
+      endpoint http://example.com
+      profile_name cfprof
+    ]
+    d.run
+
+    client = d.instance.instance_variable_get(:@client)
+    credentials = client.config.credentials
+
+    assert_equal 'ok', credentials.access_key_id
+
+  end
+
   def test_format
-    d = create_driver
+
+    d = create_driver_no_write
+
+    d.emit({'id' => 'y0234', 'type' => 'hoge'}) # ignore because type
+    d.emit({'id' => 'x1234', 'type' => 'add', 'fields' => {'foo' => 1, 'bar' => 'a'}})
+    d.emit({'id' => 'y2234', 'type' => 'delete'})
+    d.emit({'type' => 'add'}) # ignore because id is not exists
+    d.emit({'id' => 'x4234', 'type' => 'add'}) # ignore because fields is not exits
+    d.emit({'id' => 'x5234', 'type' => 'add', 'fields' => {'foo' => 3, 'bar' => 'b'}})
+    d.emit({'id' => 'x3234'}) # ignore because type is not exists
+
+    d.expect_format %[{"id":"x1234","type":"add","fields":{"foo":1,"bar":"a"}}\n]
+    d.expect_format %[{"id":"y2234","type":"delete"}\n]
+    d.expect_format %[{"id":"x5234","type":"add","fields":{"foo":3,"bar":"b"}}\n]
+
+    d.run
+
   end
 
   def test_write
+    documents = [
+      {'id' => 'x1234', 'type' => 'add', 'fields' => {'foo' => 1, 'bar' => 'a'}},
+      {'id' => 'y2234', 'type' => 'delete'},
+      {'id' => 'x5234', 'type' => 'add', 'fields' => {'foo' => 3, 'bar' => 'b'}},
+    ]
+    stub_client = Aws::CloudSearchDomain::Client.new(
+      :endpoint => 'http://example.com',
+      :stub_responses => true
+    )
+    mock(Aws::CloudSearchDomain::Client).new(:endpoint => 'http://example.com') {
+      stub_client
+    }
+    mock( stub_client ).upload_documents(
+      :content_type => "application/json",
+      :documents    => documents.to_json,
+    )
     d = create_driver
+    documents.each { |document|
+      d.emit( document )
+    }
+    d.run
   end
 end
